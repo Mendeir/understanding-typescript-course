b7481bbe9399e8b50f941701acf9f866c57c1074
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-29 20:58:18 +0800

    Initialize folder to be compiled by typescript

---
+++ understanding-ts/objs-arrays-enum.js
@@ -0,0 +1,11 @@
+"use strict";
+const person = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "Author"],
+};
+console.log(person.name);
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
