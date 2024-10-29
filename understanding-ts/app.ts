2fe331da5dc89ed3c4fbd760e0efdb4fc4daef36
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-29 19:48:25 +0800

    Try out types in an array

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,9 +1,15 @@
 const person: {
     name: string;
     age: number;
+    hobbies: string[];
 } = {
     name: "Maximilian",
     age: 30,
+    hobbies: ["Sports", "Cooking"],
 };
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
