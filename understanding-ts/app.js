9375d5ef835c148384b1544527cb6259ccfab339
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-29 19:45:02 +0800

    Try out types in a class

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,15 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var phrase = "Result is: ";
-var result = add(number1, number2, printResult, phrase);
-console.log(result);
+var person = {
+    name: "Maximilian",
+    age: 30,
+};
+console.log(person.name);
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,16 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Maximilian",
+    age: 30,
+};
 
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return n1 + n2;
-    }
-}
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const phrase = "Result is: ";
-
-add(number1, number2, printResult, phrase);
+console.log(person.name);
