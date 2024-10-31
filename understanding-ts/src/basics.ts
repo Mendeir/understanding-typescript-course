8b0aa9a58eedee6ef2440f98340abdb76b0bc13f
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-31 19:39:55 +0800

    Fix up directory structure and add no emit on error

--- understanding-ts/src/basics.ts
+++ understanding-ts/src/basics.ts
@@ -1,15 +1,23 @@
-"use strict";
-function add_basics(n1, n2, showResult, phrase) {
+function add_basics(
+    n1: number,
+    n2: number,
+    showResult: boolean,
+    phrase: string,
+) {
     const result = n1 + n2;
+
     if (showResult) {
         console.log(phrase + result);
-    }
-    else {
+    } else {
         return n1 + n2;
     }
 }
+
 const number1 = 5;
 const number2 = 2.8;
 const printResultFlag = true;
 const phrase = "Result is: ";
+
+console.log("test");
+
 add_basics(number1, number2, printResultFlag, phrase);
