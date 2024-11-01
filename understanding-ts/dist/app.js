5956668cf85e8eb5e72c6f3b56e06e8548ecb753
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-01 11:38:40 +0800

    Try out type guards

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -5,4 +5,45 @@
         privileges: ["create-server"],
         startDate: new Date(),
     };
+    function add(a, b) {
+        if (typeof a === "string" || typeof b === "string") {
+            return a.toString() + b.toString();
+        }
+        return a + b;
+    }
+    function printEmployeeInformation(emp) {
+        console.log("Name: " + emp.name);
+        if ("privileges" in emp) {
+            console.log("Privileges: " + emp.privileges);
+        }
+        if ("startDate" in emp) {
+            console.log("Privileges: " + emp.startDate);
+        }
+    }
+    printEmployeeInformation({
+        name: "MG",
+        privileges: ["retrieve-files"],
+        startDate: new Date(),
+    });
+    class Car {
+        drive() {
+            console.log("Driving...");
+        }
+    }
+    class Truck {
+        drive() {
+            console.log("Driving a truck...");
+        }
+        loadCargo(amount) {
+            console.log("Loading cargo..." + amount);
+        }
+    }
+    const v1 = new Car();
+    const v2 = new Truck();
+    function useVehicle(vehicle) {
+        vehicle.drive();
+        if ('loadCargo' in vehicle) {
+            vehicle.loadCargo(1000);
+        }
+    }
 }
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -21,4 +21,61 @@
     type Numeric = number | boolean;
 
     type Universal = Combinable & Numeric;
+
+    function add(a: Combinable, b: Combinable) {
+        if (typeof a === "string" || typeof b === "string") {
+            return a.toString() + b.toString();
+        }
+
+        return a + b;
+    }
+
+    type UnkownEmployee = Employee | Admin;
+
+    function printEmployeeInformation(emp: UnkownEmployee) {
+        console.log("Name: " + emp.name);
+
+        if ("privileges" in emp) {
+            console.log("Privileges: " + emp.privileges);
+        }
+
+        if ("startDate" in emp) {
+            console.log("Privileges: " + emp.startDate);
+        }
+    }
+
+    printEmployeeInformation({
+        name: "MG",
+        privileges: ["retrieve-files"],
+        startDate: new Date(),
+    });
+
+    class Car {
+        drive() {
+            console.log("Driving...");
+        }
+    }
+
+    class Truck {
+        drive() {
+            console.log("Driving a truck...");
+        }
+        loadCargo(amount: number) {
+            console.log("Loading cargo..." + amount);
+        }
+    }
+
+    type Vehicle = Car | Truck;
+
+    const v1 = new Car();
+    const v2 = new Truck();
+
+
+    function useVehicle(vehicle: Vehicle) {
+        vehicle.drive();
+
+        if ('loadCargo' in vehicle) {
+            vehicle.loadCargo(1000)
+        }
+    }
 }
