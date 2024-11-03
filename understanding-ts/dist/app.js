8aea052b50ca57622e4a5f11008dc0cce8b9f409
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-03 14:24:27 +0800

    Try out generics

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,83 +1,10 @@
 "use strict";
-var _a;
 {
-    const e1 = {
-        name: "MG",
-        privileges: ["create-server"],
-        startDate: new Date(),
-    };
-    function add(a, b) {
-        if (typeof a === "string" || typeof b === "string") {
-            return a.toString() + b.toString();
-        }
-        return a + b;
-    }
-    const result = add("Max", "Schwarz");
-    result.split(" ");
-    const fetchedUserData = {
-        id: "u1",
-        name: "Max",
-        job: { title: "CEO", description: "My own company" },
-    };
-    console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-    const userInput = '';
-    const storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
-    console.log(storedData);
-    function printEmployeeInformation(emp) {
-        console.log("Name: " + emp.name);
-        if ("privileges" in emp) {
-            console.log("Privileges: " + emp.privileges);
-        }
-        if ("startDate" in emp) {
-            console.log("Privileges: " + emp.startDate);
-        }
-    }
-    printEmployeeInformation({
-        name: "MG",
-        privileges: ["retrieve-files"],
-        startDate: new Date(),
+    const names = ["Max", "Manuel"];
+    names[0].split(" ");
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("This is done!");
+        }, 2000);
     });
-    class Car {
-        drive() {
-            console.log("Driving...");
-        }
-    }
-    class Truck {
-        drive() {
-            console.log("Driving a truck...");
-        }
-        loadCargo(amount) {
-            console.log("Loading cargo..." + amount);
-        }
-    }
-    const v1 = new Car();
-    const v2 = new Truck();
-    function useVehicle(vehicle) {
-        vehicle.drive();
-        if ("loadCargo" in vehicle) {
-            vehicle.loadCargo(1000);
-        }
-    }
-    useVehicle(v1);
-    useVehicle(v2);
-    function moveAnimal(animal) {
-        let speed;
-        switch (animal.type) {
-            case "bird":
-                speed = animal.flyingSpeed;
-                break;
-            case "horse":
-                speed = animal.runningSpeed;
-                break;
-        }
-        console.log("Moving at speed: " + speed);
-    }
-    moveAnimal({ type: "bird", flyingSpeed: 10 });
-    const userInputElement = document.getElementById("user-input");
-    if (userInputElement) {
-        userInputElement.value = "Hi there!";
-    }
-    const errorBag = {
-        email: "Not a valid email!",
-    };
 }
