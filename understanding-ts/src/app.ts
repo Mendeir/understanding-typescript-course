1ce6c0fdb232c7a36583a9b8207853ea9addf064
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-03 19:45:15 +0800

    Try out another generic function

--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -8,4 +8,22 @@
         { age: number }
     >({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
     console.log(mergeObj.age);
+
+    interface Lengthy {
+        length: number;
+    }
+
+    function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+        let descriptionText = "Got no value.";
+
+        if (element.length === 1) {
+            descriptionText = "Got 1 element.";
+        } else if (element.length > 1) {
+            descriptionText = "Got " + element.length + " elements.";
+        }
+
+        return [element, descriptionText];
+    }
+
+    console.log(countAndDescribe(['Sports', 'Cooking']));
 }
