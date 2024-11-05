9eb22c501e5e1d7fda5f5b98fef89beab106a99b
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-05 20:45:47 +0800

    Add cross-component communication

--- react-proj/src/components/NewTodo.tsx
+++ react-proj/src/components/NewTodo.tsx
@@ -1,12 +1,16 @@
 import React, { useRef } from "react";
 
-function NewTodo() {
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+};
+
+function NewTodo(props: NewTodoProps) {
     const textInputRef = useRef<HTMLInputElement>(null);
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
         const enteredText = textInputRef.current!.value;
-        console.log(enteredText);
+        props.onAddTodo(enteredText);
     };
 
     return (
