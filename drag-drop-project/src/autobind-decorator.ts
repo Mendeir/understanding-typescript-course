d7cd06d7b7cad6900bcedc1a2eb898b189a14b8d
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-05 11:41:19 +0800

    Delegate more of the project functionalities into a namespace

---
+++ drag-drop-project/src/autobind-decorator.ts
@@ -0,0 +1,14 @@
+namespace App {
+    export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+        const adjacentDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFunction = originalMethod.bind(this);
+                return boundFunction;
+            },
+        };
+
+        return adjacentDescriptor;
+    }
+}
