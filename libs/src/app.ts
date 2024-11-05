053f046b98f0bf94ec5cf85815fbb85632546b37
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-05 17:16:44 +0800

    Try out class-validator

--- libs/src/app.ts
+++ libs/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
 
 import { Product } from "./product.model";
 
@@ -8,9 +9,18 @@ const products = [
     { title: "A Book", price: 10.99 },
 ];
 
+const newProd = new Product(" ", -5.99);
+validate(newProd).then((errors) => {
+    if (errors.length > 0) {
+        console.log("VALIDATION ERRORS");
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
+
 const loadedProducts = plainToInstance(Product, products);
 
 for (const product of loadedProducts) {
     console.log(product.getInformation());
 }
-
