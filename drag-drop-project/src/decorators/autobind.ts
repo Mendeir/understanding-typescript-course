ccdcc7cf58d18c0281c3bb9ede75dbaeb35cc26e
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-11-05 14:32:03 +0800

    Try out ES modules for organizing files

--- drag-drop-project/src/decorators/autobind.ts
+++ drag-drop-project/src/decorators/autobind.ts
@@ -1,14 +1,12 @@
-namespace App {
-    export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-        const originalMethod = descriptor.value;
-        const adjacentDescriptor: PropertyDescriptor = {
-            configurable: true,
-            get() {
-                const boundFunction = originalMethod.bind(this);
-                return boundFunction;
-            },
-        };
+export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjacentDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFunction = originalMethod.bind(this);
+            return boundFunction;
+        },
+    };
 
-        return adjacentDescriptor;
-    }
+    return adjacentDescriptor;
 }
